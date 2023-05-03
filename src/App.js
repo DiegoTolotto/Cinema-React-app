aa9b2f661bbeaf6da00df24bdb9ad15471990214
Diego Tolotto <diego.tolotto8@gmail>
2023-05-03 15:19:29 -0300

    Trazendo primeiras informações da API

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ import React, { useEffect, useState } from 'react';
 function App() {
   const [filme, setFilmes] = useState()
 
-
+const buscaApi = () => {
+  fetch(`https://cinemaaws.onrender.com/filme`)
+    .then(res => res.json())
+    .then(res => setFilmes(res))
+}
   useEffect(() => {
     
   })
