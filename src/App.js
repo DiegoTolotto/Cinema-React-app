5df83059ec40332ade45ad28b557f5bc8ddc09ec
Diego Tolotto <diego.tolotto8@gmail>
2023-05-05 11:28:15 -0300

    corrigindo erro de syntaxe

--- src/App.js
+++ src/App.js
@@ -1,30 +1,29 @@
 import './App.css';
-import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 
 function App() {
-  const [filme, setFilmes] = useState()
+  const [filme, setFilmes] = useState([])
 
-const buscaApi = () => {
-  fetch(`https://cinemaaws.onrender.com/filme`)
-    .then((res) => res.json())
-    .then((response) => setFilmes(response))
-    .catch((e) => {
-      console.log(e)
-    })
-}
-console.log(filme[0])
+  const buscaApi = () => {
+    fetch(`https://cinemaaws.onrender.com/filme`)
+      .then((res) => res.json())
+      .then((response) => setFilmes(response))
+  }
+
   useEffect(() => {
     buscaApi()
   }, [])
 
+console.log(filme)
   return (
     <>
       <div>{filme.map((data) => {
         return(
-          <div>
+          <>
             <div>{data.nome}</div>
-          </div>
+            <div>{data.genero}</div>
+            <div>{data.classificacao}</div>
+          </>
         )
       })}</div>
 
