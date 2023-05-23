691f3d411ef53f1580225fb9ea75a6a23f1f4c9c
Diego Tolotto <diego.tolotto8@gmail>
2023-05-23 12:57:01 -0300

    Adicicionando furmulario para inseririr filmes

--- src/components/postFilme.js
+++ src/components/postFilme.js
@@ -4,11 +4,105 @@ import axios from "axios";
 import { isContentEditable } from "@testing-library/user-event/dist/utils";
 
 function PostFilme() {
- 
+  const [data, setData] = useState([])
+  const [classificacao, setClassificacao] = useState("")
+  const [descricao, setDescricao] = useState("")
+  const [genero, setGenero] = useState("")
+  const [link, setlink] = useState("")
+  const [nome, setNome] = useState("")
+  const [tempoDeDuracao, setTempoDeDuracao] = useState("")
+  const [valor, setValor] = useState("")
+
+
+  const handleSubmit = () => {
+
+    axios.post(`https://cinemaaws.onrender.com/filme`, {
+      classificacao: classificacao,
+      descricao: descricao,
+      genero: genero,
+      link: link,
+      nome: nome,
+    })
+      .then((res) => res.data)
+      .then((res) => setData(res))
+      .catch((e) => console.log(e))
+
+
+  }
 
   return (
-   <>
-   </>
+    <>
+
+      <div>
+        <form >
+          <label>classificação</label>
+          <input
+            className="campo-de-busca"
+            id="classificacao"
+            value={classificacao}
+            onChange={(e) => setClassificacao(e.target.value)}
+          ></input><br /><br />
+
+          <label>descrição</label>
+          <input
+            className="campo-de-busca"
+            id="descricao"
+            value={descricao}
+            onChange={(e) => setDescricao(e.target.value)}
+          ></input><br /><br />
+
+          <label>Genero</label>
+          <input
+            className="campo-de-busca"
+            id="genero"
+            value={genero}
+            onChange={(e) => setGenero(e.target.value)}
+          ></input><br /><br />
+
+          <label>Link</label>
+          <input
+            className="campo-de-busca"
+            id="link"
+            value={link}
+            onChange={(e) => setlink(e.target.value)}
+          ></input><br /><br />
+
+          <label>Nome</label>
+          <input
+            className="campo-de-busca"
+            id="nome"
+            value={nome}
+            onChange={(e) => setNome(e.target.value)}
+          ></input><br /><br />
+
+          <label>Tempo de duração</label>
+          <input
+            className="campo-de-busca"
+            id="tempoDeDuracao"
+            value={tempoDeDuracao}
+            onChange={(e) => setTempoDeDuracao(e.target.value)}
+          ></input><br /><br />
+
+          <label>Valor</label>
+          <input
+            className="campo-de-busca"
+            id="valor"
+            value={valor}
+            onChange={(e) => setValor(e.target.value)}
+          ></input><br /><br />
+
+          <button onClick={(e) => {
+            e.preventDefault();
+            handleSubmit();
+
+          }}>Submit</button>
+
+
+
+
+        </form>
+      </div >
+    </>
   )
 
 }
