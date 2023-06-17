74afebe808d52903f6fe8e407ea458dca1166321
Diego Tolotto <diego.tolotto8@gmail>
2023-06-16 22:19:04 -0300

    Adicionando estilização nos inputs da pagina adicinar filmes

--- src/components/postFilme.js
+++ src/components/postFilme.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { useState } from "react";
 import axios from "axios";
-import { Input } from 'antd';
+import { Input, Button } from 'antd';
 
+const { TextArea } = Input;
 
 function PostFilme() {
   const [data, setData] = useState([])
@@ -36,67 +37,76 @@ function PostFilme() {
       <div>
         <form >
           <label>Classificação</label>
-          <Input
-            showCount maxLength={20}
-            className="campo-de-busca"
-            id="classificacao"
-            value={classificacao}
-            onChange={(e) => setClassificacao(e.target.value)}
-          ></Input><br /><br />
+          <div>
+            <Input
+              showCount maxLength={20}
+              className="campo-de-busca"
+              id="classificacao"
+              value={classificacao}
+              onChange={(e) => setClassificacao(e.target.value)}
+            ></Input><br /><br />
 
-          <label>Descrição</label>
-          <input
-            className="campo-de-busca"
-            id="descricao"
-            value={descricao}
-            onChange={(e) => setDescricao(e.target.value)}
-          ></input><br /><br />
+            <label>Descrição</label>
+            <TextArea
+              showCount maxLength={100}
+              className="campo-de-busca"
+              id="descricao"
+              value={descricao}
+              onChange={(e) => setDescricao(e.target.value)}
+            ></TextArea><br /><br />
 
-          <label>Genero</label>
-          <input
-            className="campo-de-busca"
-            id="genero"
-            value={genero}
-            onChange={(e) => setGenero(e.target.value)}
-          ></input><br /><br />
+            <label>Genero</label>
+            <Input
+              showCount maxLength={20}
+              className="campo-de-busca"
+              id="genero"
+              value={genero}
+              onChange={(e) => setGenero(e.target.value)}
+            ></Input><br /><br />
+          </div>
+          <div>
+            <label>Link</label>
+            <Input
+              showCount maxLength={20}
+              className="campo-de-busca"
+              id="link"
+              value={link}
+              onChange={(e) => setlink(e.target.value)}
+            ></Input><br /><br />
 
-          <label>Link</label>
-          <input
-            className="campo-de-busca"
-            id="link"
-            value={link}
-            onChange={(e) => setlink(e.target.value)}
-          ></input><br /><br />
+            <label>Nome</label>
+            <Input
+              showCount maxLength={20}
+              className="campo-de-busca"
+              id="nome"
+              value={nome}
+              onChange={(e) => setNome(e.target.value)}
+            ></Input><br /><br />
 
-          <label>Nome</label>
-          <input
-            className="campo-de-busca"
-            id="nome"
-            value={nome}
-            onChange={(e) => setNome(e.target.value)}
-          ></input><br /><br />
+            <label>Tempo de duração</label>
+            <Input
+              showCount maxLength={20}
+              className="campo-de-busca"
+              id="tempoDeDuracao"
+              value={tempoDeDuracao}
+              onChange={(e) => setTempoDeDuracao(e.target.value)}
+            ></Input><br /><br />
 
-          <label>Tempo de duração</label>
-          <input
-            className="campo-de-busca"
-            id="tempoDeDuracao"
-            value={tempoDeDuracao}
-            onChange={(e) => setTempoDeDuracao(e.target.value)}
-          ></input><br /><br />
+            <label>Valor</label>
+            <Input
+              showCount maxLength={20}
+              className="campo-de-busca"
+              id="valor"
+              value={valor}
+              onChange={(e) => setValor(e.target.value)}
+            ></Input><br /><br />
+          </div>
 
-          <label>Valor</label>
-          <input
-            className="campo-de-busca"
-            id="valor"
-            value={valor}
-            onChange={(e) => setValor(e.target.value)}
-          ></input><br /><br />
-
-          <button onClick={(e) => {
+          <Button type="primary" onClick={(e) => {
             e.preventDefault();
             handleSubmit();
 
-          }}>Submit</button>
+          }}>Submit</Button>
 
 
 
