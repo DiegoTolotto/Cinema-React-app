51ca275969e61f425fdd2d613a06c519910bbf1b
Diego Tolotto <diego.tolotto8@gmail>
2023-06-20 12:14:44 -0300

    Definindo padrão a seguir para o Footer da home

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,16 @@
+import { Footer } from "antd/es/layout/layout";
+import React from "react";
+
+function Footer() {
+    return(
+        <div>
+            <footer>
+                <div className="homeLink"></div>
+                <div className="combosPipocas"></div>
+                <div className="contatos"></div>
+            </footer>
+        </div>
+    )
+}
+
+export default Footer;
